f66a8dc513bbe24722d7d7c16def5bdb4a97703e
PerdiDev <perdidev@gmail.com>
2023-02-28 07:19:01 +0800

    Init colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        sky: {
+          50: "#E7E7EF",
+          100: "#B8B9D6",
+          200: "#8F91BD",
+          300: "#6B6FA4",
+          400: "#4C518B",
+          500: "#323971",
+          600: "#1E2558",
+          700: "#0F163F",
+          800: "#050B2C",
+          900: "#000519",
+          950: "#00010C",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
